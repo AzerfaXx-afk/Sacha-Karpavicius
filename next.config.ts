870dfdd92a0d947ce47e56cf85eaa744f7d0e606
AzerfaXx-afk <azerfaxx@gmail.com>
2026-08-02 18:34:43 +0200

    fix: add quality 40 to next.config.ts images.qualities

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 640],
-    qualities: [30, 50, 75, 85, 90, 92, 95, 96, 100],
+    qualities: [30, 40, 50, 75, 85, 90, 92, 95, 96, 100],
   },
   compress: true,
   reactStrictMode: true,
